530937f7a290dd25c1b41422ac00d3808468573e
Gustavo Silva <ggustavo.borges13@gmail.com>
2024-09-15 23:02:27 -0300

    update general - V1.1.0

--- device.py
+++ device.py
@@ -10,6 +10,15 @@ from config import *
     Um dispositivo lógico representa uma instância dessa implementação 
     com o seu próprio estado e recursos independentes de outros dispositivos lógicos.
 """
+class QueueFamilyIndices:
+
+    def __init__(self):
+        self.graphicsFamily = None
+        self.presentFamily = None
+    
+    def is_complete(self):
+        
+        return not(self.graphicsFamily is None or self.presentFamily is None)
 
 def log_device_properties(device):
     
@@ -132,3 +141,133 @@ def choose_physical_device(instance, debug):
             return device
 
     return None
+
+def find_queue_families(device, instance, surface, debug):
+
+        indices = QueueFamilyIndices()
+        
+        surfaceSupport = vkGetInstanceProcAddr(instance, "vkGetPhysicalDeviceSurfaceSupportKHR")
+        
+        queuFamilies = vkGetPhysicalDeviceQueueFamilyProperties(device)
+
+
+        if debug:
+            print(f"{WARNING}Há {len(queuFamilies)} famílias de filas disponíveis no sistema.{RESET}")
+
+        # Verificacao bit a bit para verificar se a fila suporta nossas operacoes graficas
+        for i, queueFamily in enumerate(queuFamilies):
+
+            """
+            // Provided by VK_VERSION_1_0
+                typedef struct VkQueueFamilyProperties {
+                VkQueueFlags    queueFlags;
+                uint32_t        queueCount;
+                uint32_t        timestampValidBits;
+                VkExtent3D      minImageTransferGranularity;
+                } VkQueueFamilyProperties;
+
+            // Provided by VK_VERSION_1_0
+            typedef enum VkQueueFlagBits {
+                VK_QUEUE_GRAPHICS_BIT = 0x00000001,
+                VK_QUEUE_COMPUTE_BIT = 0x00000002,
+                VK_QUEUE_TRANSFER_BIT = 0x00000004,
+                VK_QUEUE_SPARSE_BINDING_BIT = 0x00000008,
+                // Provided by VK_VERSION_1_1
+                VK_QUEUE_PROTECTED_BIT = 0x00000010,
+            } VkQueueFlagBits;
+
+            As famílias de filas no Vulkan são categorizadas por tipos de operações que suportam, como:
+            - Gráficos: Para renderização gráfica.
+            - Computação: Para cálculos gerais em GPU, sem necessariamente envolver gráficos.
+            - Transferência de dados: Para movimentar dados entre a memória da CPU e GPU.
+            - Memória esparsa: Para gerenciar memória de maneira otimizada, como em alocação dinâmica.
+            - Memória protegida: Para operações que envolvem o uso de memória protegida, garantindo segurança e isolamento de dados sensíveis dentro da GPU.
+
+            """
+
+            if queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT:
+                indices.graphicsFamily = i
+
+                if debug:
+                    print(f"{OKGREEN}A família de filas {i} é adequada para gráficos.{RESET}")
+
+            if surfaceSupport(device, i, surface):
+                indices.presentFamily = i
+
+                if debug:
+                    print(f"{OKGREEN}A família de filas {i} é adequada para apresentar.{RESET}")
+
+            if indices.is_complete():
+                break
+
+        return indices
+
+def create_logical_device(physicalDevice, instance, surface, debug):
+
+    """
+        Criar uma abstração em torno da GPU
+    """
+
+    """
+        No momento da criação, todas as filas necessárias também serão criadas,
+        portanto, as informações de criação de fila devem ser passadas em
+    """
+
+    indices = find_queue_families(physicalDevice, instance, surface, debug)
+    uniqueIndices = [indices.graphicsFamily,]
+    if indices.graphicsFamily != indices.presentFamily:
+            uniqueIndices.append(indices.presentFamily)
+
+    queueCreateInfo = []
+
+    for queueFamilyIndex in uniqueIndices:
+        queueCreateInfo.append(
+            VkDeviceQueueCreateInfo(
+                queueFamilyIndex = queueFamilyIndex,
+                queueCount = 1,
+                pQueuePriorities= [1.0,]
+            )
+        )
+
+    """
+        Os recursos do dispositivo devem ser solicitados antes que o dispositivo seja abstraído,
+        portanto, pagamos apenas pelo que usamos
+    """
+    devicesFeatures = VkPhysicalDeviceFeatures()
+
+    enabledLayers = []
+    if debug:
+        enabledLayers.append("VK_LAYER_KHRONOS_validation")
+
+    createInfo = VkDeviceCreateInfo(
+        queueCreateInfoCount = len(queueCreateInfo), 
+        pQueueCreateInfos = queueCreateInfo,
+        enabledExtensionCount = 0,
+        pEnabledFeatures = [devicesFeatures,],
+        enabledLayerCount = len(enabledLayers), 
+        ppEnabledLayerNames = enabledLayers
+    )
+
+    # Se pegarmos algo "[createInfo,] e embrulharmos/wrapper em uma lista, a API do vulkan vai 
+    # entender como um ponteiro"
+    return vkCreateDevice(
+        physicalDevice = physicalDevice, 
+        pCreateInfo = [createInfo,], 
+        pAllocator = None
+    )
+   
+def get_queues(physicalDevice, logicalDevice, instance, surface, debug):
+
+    indices = find_queue_families(physicalDevice, instance, surface, debug)
+    return [
+        vkGetDeviceQueue(
+            device = logicalDevice,
+            queueFamilyIndex = indices.graphicsFamily,
+            queueIndex = 0
+        ),
+        vkGetDeviceQueue(
+            device = logicalDevice,
+            queueFamilyIndex = indices.presentFamily,
+            queueIndex = 0
+        ),
+    ]
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import logging
 import device
 import time
 
+# https://registry.khronos.org/vulkan/specs/1.3/html/ documentacao vulkan
+
 class Engine:
 
     def __init__(self):
@@ -37,10 +39,10 @@ class Engine:
         self.window = glfw.create_window(self.width, self.height, self.glfw_title_name, None, None)
         if self.window is not None:
             if self.debugMode:
-                print(f"{OKGREEN}Successfully made a glfw window called {self.glfw_title_name}, width: {self.width}, height: {self.height}{RESET}\n")
+                print(f"{OKGREEN}Foi criada com êxito uma janela glfw chamada {self.glfw_title_name}, largura: {self.width}, altura: {self.height}{RESET}\n")
         else:
             if self.debugMode:
-                print(f"{FAIL}GLFW window creation failed{RESET}\n")
+                print(f"{FAIL}Falha na criação da janela GLFW{RESET}\n")
   
     def make_instance(self):
 
@@ -48,16 +50,61 @@ class Engine:
 
         if self.debugMode:
             self.debugMessenger = logging.make_debug_messenger(self.instance)
-    
+
+        #criar a surface na janela
+        c_style_surface = ffi.new("VkSurfaceKHR*")
+        if(
+            glfw.create_window_surface(
+                instance = self.instance,
+                window = self.window,
+                allocator = None,
+                surface = c_style_surface
+            ) != VK_SUCCESS
+        ):
+            if self.debugMode:
+                print(f"{FAIL}Falha ao abstrair a superfície do glfw para o Vulkan{RESET}")
+        elif self.debugMode:
+            print(f"{OKGREEN}Sucesso na abstração da superfície do glfw para o Vulkan{RESET}")
+        
+        self.surface = c_style_surface[0]
+        
     def make_device(self):
 
+        #escolhe o dispositivo fisico
         self.physicalDevice = device.choose_physical_device(self.instance, self.debugMode)
+        
+
+        #envolve a gpu em um dispositivo logico para podermos nos comunicar com ele
+            #device.find_queue_families(self.physicalDevice, self.debugMode)
+        self.device = device.create_logical_device(
+            self.physicalDevice, self.instance, self.surface, self.debugMode)
+
+        #obter fila de gráficos, para que possamos fazer o trabalho com gráficos
+        (self.graphicsQueue, self.presentQueue) = device.get_queues(
+            physicalDevice = self.physicalDevice, 
+            logicalDevice = self.device, 
+            instance = self.instance, 
+            surface = self.surface,
+            debug = self.debugMode
+        )
 
     def close(self):
 
         if self.debugMode:
             print(f"{HEADER}\nAté logo!\n{RESET}")
         
+        vkDestroyDevice(
+            device = self.device, 
+            pAllocator = None
+        )
+
+        destructionFunction = vkGetInstanceProcAddr(self.instance, 'vkDestroySurfaceKHR')
+        destructionFunction(
+            instance = self.instance, 
+            surface = self.surface, 
+            pAllocator = None
+        )
+
         if self.debugMode:
             #função de destruição de busca
             destructionFunction = vkGetInstanceProcAddr(self.instance, 'vkDestroyDebugReportCallbackEXT')
@@ -69,7 +116,11 @@ class Engine:
                     ,pAllocator
                 ,):
             """
-            destructionFunction(self.instance, self.debugMessenger, None)
+            destructionFunction(
+                self.instance, 
+                self.debugMessenger, 
+                None
+            )
 
         """
             from _vulkan.py:
@@ -79,10 +130,12 @@ class Engine:
                 pAllocator,
             )
         """
-        vkDestroyInstance(self.instance, None)
+        vkDestroyInstance(
+            self.instance, 
+            None
+        )
 
 	    #encerrar o glfw
-        time.sleep(5)
         glfw.terminate()
 
 if __name__ == "__main__":
